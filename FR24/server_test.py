b815e6fb8b1a7160a26d1b7b5dff364eddd71983
Iron237 <1650655517@QQ.COM>
2024-05-09 18:22:20 +0800

    Initial commit

---
+++ FR24/server_test.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template,request
+import requests 
+import fr24爬虫test as frpc
+app = Flask(__name__, template_folder='C:\\Users\\EddieChen\\Documents\\works\\clear workspace\\FR24\\template')
+
+@app.route('/')
+def home():
+    return render_template('FR24_flight_data.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    message = request.form['message']
+    
+    return '消息已收到'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1145, debug=True)
